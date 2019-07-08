53185b111edf395b255c8596bef65de4ad1b97fa
foxugly <rvilain@skynet.be>
2019-07-08 09:33:13 +0200

    sample ok

--- sample.py
+++ sample.py
@@ -1,10 +1,16 @@
-from selenium import webdriver
-import time
+#!/usr/bin/env python3
 
-driver = webdriver.Chrome()
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+
+dir = os.path.dirname(os.path.abspath(__file__))
+driver = webdriver.Chrome(os.path.join(dir,'chromedriver'))
 driver.get('http://www.google.com')
 search = driver.find_element_by_name('q')
 search.send_keys("google search through python")
 search.send_keys(Keys.RETURN)
 time.sleep(10)
+driver.close()
 driver.quit()
