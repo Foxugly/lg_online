b6cab9caf255d230d0e728267788ff5f7e848652
Ubuntu <ubuntu@ip-172-31-29-58.eu-west-1.compute.internal>
2019-07-15 09:30:21 +0000

    bug fixed

--- customuser/validators.py
+++ customuser/validators.py
@@ -7,7 +7,7 @@ def validate_enterprise_numberl(value):
     reg_ok = re.compile("[0-9 -.]{9,12}")
     if len(re.findall(reg, value)):
         raise ValidationError("no character in the field")
-    elif len(re.findall(reg_ok, value):
+    elif len(re.findall(reg_ok, value)):
         return value
     else:
         raise ValidationError("unknown characters")
