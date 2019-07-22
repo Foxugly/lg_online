27c65ba8f1c0a6502af840ff78be02466f39e465
foxugly <rvilain@skynet.be>
2019-07-22 22:01:20 +0200

    sprint2 mail

--- customuser/views.py
+++ customuser/views.py
@@ -15,6 +15,7 @@ from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
 from customuser.tokens import account_activation_token
 from django.contrib.auth.forms import AuthenticationForm
+from contact.models import Contact
 
 
 def activate(request, uidb64, token):
@@ -25,6 +26,7 @@ def activate(request, uidb64, token):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
+        user.contact = Contact.objects.filter(default=True)[0]
         user.save()
         messages.success(request, 'Thank you for your email confirmation. Now you can login your account.')
         return redirect('home')
