1b47e8fcaab32c86ca22192ef5bab7376e73595c
Foxugly <rvilain@skynet.be>
2019-07-24 00:39:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
-sudo apt-get install python3-pip
-sudo pip3 install virtualenv
-cd ~/mlg/
-virtualenv venv -p python3
-source venv/bin/activate
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3
+sudo apt-get install python3-pip python3-venv
+sudo apt-get apache2 libapache2-mod-wsgi-py3 
+cd ~
+mkdir mlg
+cd mlg
 git clone https://github.com/Foxugly/lg_online.git
+python3 -m venv venv
 source venv/bin/activate
-cd lg_online
+cd  lg_online
 pip install -r requirements.txt
-nano lg/settings.py
-touch lg/credential_email.py
-./manage.py makemigrations
-./manage.py migrate
-./manage.py createsuperuser
+sudo cp lg_online80.conf /etc/apache2/sites-enabled/mlg.conf
+sudo ln /etc/apache2/sites-available/mlg.conf /etc/apache2/sites-enabled/mlg.conf -s
+./run.sh
+deactivate
+sudo chown -R www-data:www-data *
+sudo service apache2 restart
