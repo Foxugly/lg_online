d917da8e045598ddf200092a59c6fbc3c6c99c17
rdv <rvilain@skynet.be>
2019-07-26 12:49:43 +0200

    add name and id for selenium

--- sample.py
+++ sample.py
@@ -2,15 +2,35 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 import os
 
-dir = os.path.dirname(os.path.abspath(__file__))
-driver = webdriver.Chrome(os.path.join(dir,'chromedriver'))
-driver.get('http://www.google.com')
-search = driver.find_element_by_name('q')
-search.send_keys("google search through python")
-search.send_keys(Keys.RETURN)
+
+dir = os.path.dirname(os.path.abspath('__file__'))
+driver = webdriver.Chrome()
+driver.get('http://www.mylieutenantguillaume.com')
+search = driver.find_element_by_name('subscribe')
+search.click()
+search = driver.find_element_by_name('email')
+search.send_keys("test_mlg_acc@yopmail.com")
+search = driver.find_element_by_name('first_name')
+search.send_keys("John")
+search = driver.find_element_by_name('last_name')
+search.send_keys("Smith")
+search = driver.find_element_by_name('password1')
+search.send_keys("azerty123")
+search = driver.find_element_by_name('password2')
+search.send_keys("azerty123")
 time.sleep(10)
+search = driver.find_element_by_name('submit')
+search.click()
+driver.get('http://www.yopmail.com/')
+search = driver.find_element_by_name('login')
+search.send_keys("test_mlg_acc")
+search.send_keys(Keys.RETURN)
+search = driver.find_element_by_id('mailmillieu')
+search = driver.find_elements(By.ID, 'mailmillieu')
+search.send_keys()
 driver.close()
 driver.quit()
