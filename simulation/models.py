05a930a8328e27a8f3ef2bb7b1bed265a63705d7
Foxugly <rvilain@skynet.be>
2019-09-12 11:17:55 +0200

    Update models.py

--- simulation/models.py
+++ simulation/models.py
@@ -32,7 +32,8 @@ coef_tax_liability = {'monthly': 1.3, 'quarterly':1, 'none': 0.8}
 
 
 class Simulation(GenericClass):
-    nb_documents = models.PositiveIntegerField(_("Nombre de documents annuels (factures de vente, factures d'achat et tickets)"))
+    nb_invoices_sale = models.PositiveIntegerField(_("Nombre de factures de vente annuels"))
+    nb_invoices_purchase = models.PositiveIntegerField(_("Nombre de factures d'achat et tickets annuels"))
     nb_managers =  models.PositiveIntegerField(_("Nombre de dirigeants"))
     nb_employees = models.PositiveIntegerField(_("Nombre d'employers"))
     nb_creditcard = models.PositiveIntegerField(_("Nombre de carte de credits"))
@@ -41,12 +42,13 @@ class Simulation(GenericClass):
     tax_liability = models.CharField(max_length=20, choices=TAX_LIABILITY_CHOICES)
     
     def compute(self):
-        V_nb_docs = 1.7 * self.nb_documents
+        V_nb_invoices_sale = 0.7 * self.nb_invoices_sale
+        V_nb_invoices_purchase = 2.5 * self.nb_invoices_purchase
         V_nb_managers = 360 * self.nb_managers
         V_nb_employees = 0 if self.nb_employees == 0 else (100 * self.nb_employees) + 100
         V_nb_creditcard = 160 * self.nb_creditcard
         V_alternatif_payments = 0 if not self.alternatif_payments else 600
-        s = V_nb_docs + V_nb_managers + V_nb_employees + V_nb_creditcard + V_alternatif_payments
+        s = V_nb_invoices_sale + V_nb_invoices_purchase + V_nb_managers + V_nb_employees + V_nb_creditcard + V_alternatif_payments
         max_calulated = (s * coef_sector[self.sector] * coef_tax_liability[self.tax_liability])/12
         return max(200, math.ceil(max_calulated)), max_calulated
 
