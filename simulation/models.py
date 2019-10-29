a6b1937531161aa710042f2400ec79817b89e5e0
Foxugly <rvilain@skynet.be>
2019-10-29 12:55:59 +0100

    Update models.py

--- simulation/models.py
+++ simulation/models.py
@@ -19,8 +19,8 @@ SECTOR_CHOICES = (
     ('marketing', _("Marketing")),    
 )
 
-coef_sector = {'horeca':0.8, 'management':1, 'liberal':1.1, 'construction':0.9, 'immo':1.2, 'assurances': 1.1, 'informatique': 1.2, 
-        'retails': 0.7, 'avocat': 1.4, 'design': 1.1, 'architecte': 1, 'marketing' : 1.1}
+coef_sector = {'horeca':0.95, 'management':1.15, 'liberal':1.2, 'construction':1.05, 'immo':1.15, 'assurances': 1.25, 'informatique': 1.3, 
+        'retails': 0.95, 'avocat': 1.2, 'design': 1.3, 'architecte': 1.15, 'marketing' : 1.1}
 
 TAX_LIABILITY_CHOICES = (
     ('monthly', _("mensuellement")),
@@ -28,10 +28,18 @@ TAX_LIABILITY_CHOICES = (
     ('none', _("Non déposant")),
 )
 
-coef_tax_liability = {'monthly': 1.3, 'quarterly':1, 'none': 0.8}
+TRANSMISSION_CHOICES = (
+    ('online', _("En ligne")),
+    ('paper', _("Papier")),
+    
+)
+
+coef_tax_liability = {'monthly': 1.5, 'quarterly':1.3, 'none': 1}
 
 
 class Simulation(GenericClass):
+    turnover =  models.PositiveIntegerField(_("Chiffre d'affaires"))
+    transmission = models.CharField(_("Mode de transmission des documents"), max_length=20, choices=TRANSMISSION_CHOICES)
     nb_invoices_sale = models.PositiveIntegerField(_("Nombre de factures de vente annuels"))
     nb_invoices_purchase = models.PositiveIntegerField(_("Nombre de factures d'achat et tickets annuels"))
     nb_managers =  models.PositiveIntegerField(_("Nombre de dirigeants"))
@@ -43,14 +51,26 @@ class Simulation(GenericClass):
     created = models.DateTimeField(auto_now_add=True)
     
     def compute(self):
+        V_CA = 0
+        if turnover >= 1000000:
+            V_CA = 1.5
+        elif turnover >= 750000:
+            V_CA = 1.4
+        elif turnover >= 500000:
+            V_CA = 1.3
+        elif turnover >= 250000:
+            V_CA = 1.2
+        else:
+            V_CA = 1
         V_nb_invoices_sale = 0.7 * self.nb_invoices_sale
         V_nb_invoices_purchase = 2.5 * self.nb_invoices_purchase
-        V_nb_managers = 360 * self.nb_managers
-        V_nb_employees = 0 if self.nb_employees == 0 else (100 * self.nb_employees) + 100
-        V_nb_creditcard = 160 * self.nb_creditcard
-        V_alternatif_payments = 0 if not self.alternatif_payments else 600
+        V_nb_managers = 1500
+        V_nb_employees = 100 * self.nb_employees
+        V_nb_creditcard = 360 * self.nb_creditcard
+        V_transmission = 480 if self.transmission == "paper" else 0
+        V_alternatif_payments = 0 if not self.alternatif_payments else 300
         s = V_nb_invoices_sale + V_nb_invoices_purchase + V_nb_managers + V_nb_employees + V_nb_creditcard + V_alternatif_payments
-        max_calulated = (s * coef_sector[self.sector] * coef_tax_liability[self.tax_liability])/12
+        max_calulated = (s * V_CA * coef_sector[self.sector] * coef_tax_liability[self.tax_liability])/12
         return max(200, math.ceil(max_calulated)), max_calulated
 
     def __str__(self):
