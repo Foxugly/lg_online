4e4aa834c5b41d007e1365c80bf8fa35ef1e92e0
foxugly <rvilain@skynet.be>
2019-11-15 14:36:09 +0100

    iban algo

--- company/models.py
+++ company/models.py
@@ -78,3 +78,27 @@ class Iban(GenericClass):
 
     class Meta:
         verbose_name = _('Iban')
+
+
+# import string
+# LETTERS = {ord(d): str(i) for i, d in enumerate(string.digits + string.ascii_uppercase)}
+
+# def _number_iban(iban):
+#     return (iban[4:] + iban[:4]).translate(LETTERS)
+
+
+# def generate_iban_check_digits(iban):
+#     number_iban = _number_iban(iban[:2] + '00' + iban[4:])
+#     return '{:0>2}'.format(98 - (int(number_iban) % 97))
+
+
+# def valid_iban(iban):
+#     return int(_number_iban(iban)) % 97 == 1
+
+
+# if __name__ == '__main__':
+#     my_iban = 'RO13RZBR0000060007134800'
+#     if generate_iban_check_digits(my_iban) == my_iban[2:4] and valid_iban(my_iban):
+#         print('IBAN ok!\n')
+#     else:
+#         print('IBAN not ok!\n')
